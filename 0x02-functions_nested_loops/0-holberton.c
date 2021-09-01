b0d7275d2535a00a04f232f67f03962866f6f4c4
EmmaOkafor <okaforifebuche26@gmail.com>
2021-09-01 06:35:30 -0700

    corrected file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
+	_putchar("H");
+	_putchar("o");
+	_putchar("l");
+	_putchar("b");
+	_putchar("e");
+	_putchar("r");
+	_putchar("t");
+	_putchar("o");
 	_putchar("n \n");
 	return (0);
 }
