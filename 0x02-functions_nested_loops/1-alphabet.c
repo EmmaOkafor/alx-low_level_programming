f2a7d16d8ace556e5b4f8bab41f8d6850dfcaad9
EmmaOkafor <okaforifebuche26@gmail.com>
2021-09-01 10:02:11 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * 
+ */
+
+void print_alphabet(void)
+{
+  char b = 'a';
+   while ( b <= 'z')
+     {
+         _putchar(b);
+	 
+         b++;
+
+     }
+
+   _putchar('\n');
+
+ }
