b611a228db5a4312bdfa14c1a5c165dada53d8c0
EmmaOkafor <okaforifebuche26@gmail.com>
2021-09-01 10:52:50 -0700

    betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line
- * 
+ *print_alphabet - prints the alphabet, in lowercase, followed by a new line 
  */
 
-void print_alphabet(void) {
+void print_alphabet(void)
+{
   char b = 'a';
-   while (b <= 'z') {
+   while (b <= 'z')
+     {
        _putchar(b);
        b++;
      }
