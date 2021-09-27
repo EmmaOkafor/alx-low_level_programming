076617cadfe41c767a05d5b35795d5884f314319
EmmaOkafor <okaforifebuche26@gmail.com>
2021-09-27 10:17:33 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,51 +1,28 @@
 #include <stdio.h>
-
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with
- *        1 and 2, separated by a comma followed by a space.
+ * main - Gets the sum of even Fibonnaci numbers up to 4000000.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-  int count;
-  unsigned long fib1 = 0, fib2 = 1, sum;
-  unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-  unsigned long half1, half2;
+	unsigned long i = 0, j = 1, sum;
+	float total;
 
-  for (count = 0; count < 92; count++)
-    {
-      sum = fib1 + fib2;
-      printf("%lu, ", sum);
-
-      fib1 = fib2;
-      fib2 = sum;
-    }
-
-  fib1_half1 = fib1 / 10000000000;
-  fib2_half1 = fib2 / 10000000000;
-  fib1_half2 = fib1 % 10000000000;
-  fib2_half2 = fib2 % 10000000000;
-
-  for (count = 93; count < 99; count++)
-    {
-      half1 = fib1_half1 + fib2_half1;
-      half2 = fib1_half2 + fib2_half2;
-      if (fib1_half2 + fib2_half2 > 9999999999)
+	while (1)
 	{
-	  half1 += 1;
-	  half2 %= 10000000000;
+		sum = i + j;
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total += sum;
+
+		i = j;
+		j = sum;
 	}
+	printf("%.0f\n", total);
 
-      printf("%lu%lu", half1, half2);
-      if (count != 98)
-	printf(", ");
-
-      fib1_half1 = fib2_half1;
-      fib1_half2 = fib2_half2;
-      fib2_half1 = half1;
-      fib2_half2 = half2;
-    }
-  printf("\n");
-  return (0);
+	return (0);
 }
